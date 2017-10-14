d208624776961429d78d686eb270138731c90a0a
Dean Goldman <deanjgoldman@gmail.com>
2017-10-14 12:22:34 -0400

    reorganizing directory structure for static files

--- app/views.py
+++ app/views.py
@@ -4,3 +4,14 @@ from . import app
 @app.route('/')
 def index():
     return render_template('map3.html')
+
+@app.route('/projects')
+def projects():
+    return render_template('projects.html')
+
+@app.route('/<member>')
+def memberpage(member):
+    if member in members:
+        return render_template(url_for( member ))
+    else:
+        return 'Member not found'
