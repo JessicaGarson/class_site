e7036eab0bf9cd2dda35756a5f3e7d3b6e1117ac
Dean Goldman <deanjgoldman@gmail.com>
2017-10-28 13:31:02 -0400

    deleting map3.css and map3.js

--- app/static/map3.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-$( "button" ).each(function( index ) {
-  var num1 = Math.random();
-  var num2 = Math.random();
-  var num3 = Math.random();
-  var num4 = Math.random();
-$( this ).css( {'left': num1, 'top': num2, 'right': num3, 'bottom': num4} ) });
-});
